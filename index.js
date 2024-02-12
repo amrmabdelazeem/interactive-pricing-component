6d67151f66932a30dd4931cb63d8cc3dafe5d7eb
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-12 18:35:42 +0300

    Create function switch and eventlisteners

--- index.js
+++ index.js
@@ -1,41 +1,79 @@
 const sliderEl = document.getElementById("slider");
 const subscriberEl = document.getElementById("subscribers");
-const subscribtionEl = document.getElementById("subscribtion"); 
+const subscribtionEl = document.getElementById("subscribtion");
+const toggleEl = document.getElementById("toggle");
+var isChecked = false;
+var currentSliderValue = sliderEl.value;
 
+toggleEl.addEventListener("change", (e) => {
+  isChecked = e.target.checked;
+  switchBilling(isChecked, currentSliderValue);
+});
 
-
-sliderEl.addEventListener("input", (e)=>{
-  const currentSliderValue = e.target.value;
-
-  switch(currentSliderValue){
-    case "0":
-        subscriberEl.textContent = "10K";
-        subscribtionEl.textContent = "$8.00 "
-        break;
-    case "25":
-        subscriberEl.textContent = "50K";
-        subscribtionEl.textContent = "$12.00 "
-        break;
-    case "50":
-        subscriberEl.textContent = "100K";
-        subscribtionEl.textContent = "$16.00 "
-        break;
-    case "75":
-        subscriberEl.textContent = "500K";
-        subscribtionEl.textContent = "$24.00 "
-        break;
-    case "100":
-        subscriberEl.textContent = "1M"
-        subscribtionEl.textContent = "$36.00 "
-        break;
-
-    default:
-        subscriberEl.textContent = "100K"
-        subscribtionEl.textContent = "$8.00 "
-  }
-
+sliderEl.addEventListener("input", (e) => {
+  currentSliderValue = e.target.value;
+  switchBilling(isChecked, currentSliderValue);
 
   const progress = (currentSliderValue / sliderEl.max) * 100;
   sliderEl.style.background = `linear-gradient(to right, #a5f3eb ${progress}%, #eaeefb ${progress}%)`;
-})
+});
 
+function switchBilling(checkState, sliderValue) {
+  switch (checkState) {
+    case false:
+      switch (sliderValue) {
+        case "0":
+          subscriberEl.textContent = "10K";
+          subscribtionEl.textContent = "$8.00 ";
+          break;
+        case "25":
+          subscriberEl.textContent = "50K";
+          subscribtionEl.textContent = "$12.00 ";
+          break;
+        case "50":
+          subscriberEl.textContent = "100K";
+          subscribtionEl.textContent = "$16.00 ";
+          break;
+        case "75":
+          subscriberEl.textContent = "500K";
+          subscribtionEl.textContent = "$24.00 ";
+          break;
+        case "100":
+          subscriberEl.textContent = "1M";
+          subscribtionEl.textContent = "$36.00 ";
+          break;
+
+        default:
+          subscriberEl.textContent = "100K";
+          subscribtionEl.textContent = "$8.00 ";
+      }
+      break;
+    case true:
+      switch (sliderValue) {
+        case "0":
+          subscriberEl.textContent = "10K";
+          subscribtionEl.textContent = "$72.00";
+          break;
+        case "25":
+          subscriberEl.textContent = "50K";
+          subscribtionEl.textContent = "$108.00";
+          break;
+        case "50":
+          subscriberEl.textContent = "100K";
+          subscribtionEl.textContent = "$144.00";
+          break;
+        case "75":
+          subscriberEl.textContent = "500K";
+          subscribtionEl.textContent = "$216.00";
+          break;
+        case "100":
+          subscriberEl.textContent = "1M";
+          subscribtionEl.textContent = "$324.00";
+          break;
+
+        default:
+          subscriberEl.textContent = "100K";
+          subscribtionEl.textContent = "$144.00";
+      }
+  }
+}
